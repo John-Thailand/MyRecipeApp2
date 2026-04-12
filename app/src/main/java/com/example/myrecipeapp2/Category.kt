d94dd83642c735a63c4ea6aef6d999c8c241d91d
John-Thailand <naoki.honda@mirrorfit.jp>
2026-04-12 14:59:47 +0900

    first commit

---
+++ app/src/main/java/com/example/myrecipeapp2/Category.kt
@@ -0,0 +1,10 @@
+package com.example.myrecipeapp2
+
+data class Category (
+    val idCategory: String,
+    val strCategory: String,
+    val strCategoryThumb: String,
+    val strCategoryDescription: String
+)
+
+data class CategoriesResponse(val categories: List<Category>)
